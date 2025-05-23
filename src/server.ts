17321dcef60a25e1711f197c7ede9e69feb979d6
Marcos Vinícius Marcon <marconvcm@gmail.com>
2025-05-23 11:38:38 +0200

    Update server.ts to gracefully shutdown the server

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,26 @@
+import mongoose from "mongoose";
 import app from "./app";
 import logger from "./utils/logger";
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-   logger.info(`Server running at http://localhost:${PORT}`);
+const server = app.listen(process.env.PORT || 3000, () => {
+   logger.info(`Server started on port ${process.env.PORT || 3000}`);
 });
+
+async function shutdown(signal: string) {
+   logger.info(`received ${signal}, shutting down gracefully...`);
+   await mongoose.connection.close();
+   logger.info('database connection closed.');
+   
+   server.close(() => {
+      logger.info('leaderboard-api server closed.');
+      process.exit(0);
+   });
+   
+   setTimeout(() => {
+      logger.error('forcefully shutting down.');
+      process.exit(1);
+   }, 10000);
+}
+
+process.on('SIGTERM', () => shutdown('SIGTERM'));
+process.on('SIGINT', () => shutdown('SIGINT'));
